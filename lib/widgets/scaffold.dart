be56b8b526f56d1a8f03fe0e23b4f8007495fb85
Marcos Alejandro <cypherchabon@gmail.com>
2023-05-05 21:34:37 -0300

    feat: Impl CustomScaffold

---
+++ lib/widgets/scaffold.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../theme.dart';
+
+class CustomScaffold extends StatelessWidget {
+  final Widget body;
+
+  const CustomScaffold({
+    Key? key,
+    required this.body,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: const SystemUiOverlayStyle(
+            statusBarColor: primaryColor,
+            statusBarIconBrightness: Brightness.light,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: body,
+          ),
+        ),
+      );
+}
