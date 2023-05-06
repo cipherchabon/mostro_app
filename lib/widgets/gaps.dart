834f294b6c120ce8c053f59c57420ad3a2770dd5
Marcos Alejandro <cypherchabon@gmail.com>
2023-05-05 21:38:21 -0300

    feat: Impl gaps

---
+++ lib/widgets/gaps.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Gap widgets for vertical spacing.
+const gapH4 = SizedBox(height: 4);
+const gapH8 = SizedBox(height: 8);
+const gapH16 = SizedBox(height: 16);
+const gapH32 = SizedBox(height: 32);
+const gapH64 = SizedBox(height: 64);
+
+/// Gap widgets for horizontal spacing.
+const gapW4 = SizedBox(width: 4);
+const gapW8 = SizedBox(width: 8);
+const gapW16 = SizedBox(width: 16);
